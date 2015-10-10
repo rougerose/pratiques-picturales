0c16c24b6dc9c117b00383d360fdaaa66b65126c
christophe <christophe@rouge-rose.net>
2015-10-10 15:17:36 +0200

    icon-next

--- lang/pratiques_picturales_fr.php
+++ lang/pratiques_picturales_fr.php
@@ -4,32 +4,35 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
   // A
-  'article_pdf' => 'Télécharger la version PDF de cet article',
-  'article_pdf_court' => 'Version PDF',
-  'articles_connexes' => 'Lire également',
+  'article_pdf'            => 'Télécharger la version PDF de cet article',
+  'article_pdf_court'      => 'Version PDF',
+  'articles_connexes'      => 'Lire également',
 
   // B
-  'bibliographie' => 'Bibliographie',
+  'bibliographie'          => 'Bibliographie',
 
   // C
-  'citer_article' => 'Pour citer cet article',
+  'citer_article'          => 'Pour citer cet article',
 
   // D
-  'documents' => 'Documents',
+  'documents'              => 'Documents',
 
   // F
-  'footer_co_editorial' => 'Le comité éditorial',
+  'footer_co_editorial'    => 'Le comité éditorial',
   'footer_co_scientifique' => 'Le comité scientifique',
-  'footer_equipe' => "L'équipe",
-  'footer_revue' => 'La revue',
+  'footer_equipe'          => "L'équipe",
+  'footer_revue'           => 'La revue',
 
   // I
-  'info_document' => 'Document',
-  'issue_last' => 'dernier numéro',
+  'info_document'          => 'Document',
+  'issue_last'             => 'dernier numéro',
+
+  // L
+  'lire_sommaire'          => 'Lire le sommaire',
 
 
   // M
-  'mention_affiliation' => "Pratiques picturales est un programme de recherches",
+  'mention_affiliation'    => "Pratiques picturales est un programme de recherches",
 
   // N
   'nom_menu_objet'         => 'Plugin Pratiques picturales : Article, rubrique ou autre objet SPIP',
@@ -44,7 +47,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
   'resume'                 => 'Résumé',
 
   // T
-  'tags' => 'Mots-clés',
+  'tags'                   => 'Mots-clés',
   'texte_integral'         => 'Texte intégral',
 
 );
